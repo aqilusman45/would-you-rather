61302fdf2942cfc21e037e5c223a5b652df50089
aqilusman45 <aqilusman45@gmail.com>
2020-04-09 02:21:00 +0500

    refactor: update avatar urls

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -1,10 +1,8 @@
-let avatarPlaceHolder =
-  "https://www.pngitem.com/pimgs/m/506-5067022_sweet-shap-profile-placeholder-hd-png-download.png";
 let users = {
   sarahedo: {
     id: "sarahedo",
     name: "Sarah Edo",
-    avatarURL: avatarPlaceHolder,
+    avatarURL: "https://randomuser.me/api/portraits/women/76.jpg",
     answers: {
       "8xf0y6ziyjabvozdd253nd": "optionOne",
       "6ni6ok3ym7mf1p33lnez": "optionTwo",
@@ -16,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: "tylermcginnis",
     name: "Tyler McGinnis",
-    avatarURL: avatarPlaceHolder,
+    avatarURL: "https://randomuser.me/api/portraits/men/5.jpg",
     answers: {
       vthrdm985a262al8qx3do: "optionOne",
       xj352vofupe1dqz9emx13r: "optionTwo",
@@ -26,7 +24,7 @@ let users = {
   johndoe: {
     id: "johndoe",
     name: "John Doe",
-    avatarURL: avatarPlaceHolder,
+    avatarURL: "https://randomuser.me/api/portraits/men/54.jpg",
     answers: {
       xj352vofupe1dqz9emx13r: "optionOne",
       vthrdm985a262al8qx3do: "optionTwo",
