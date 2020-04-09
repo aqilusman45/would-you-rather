31be884f4a30a0d57e31372325253a1b50c3e233
aqilusman45 <aqilusman45@gmail.com>
2020-04-09 11:08:39 +0500

    feat: global styles

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./components/App.js";
 import { Provider } from "react-redux";
 import store from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import "./index.css";
 ReactDOM.render(
   <Provider store={store}>
     <Router>
