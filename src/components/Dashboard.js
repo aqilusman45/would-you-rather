d550ab82ad6c35f4961cfa75abe0429e7ac74658
aqilusman45 <aqilusman45@gmail.com>
2020-04-12 23:40:40 +0500

    refactor: nav logic

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,32 @@
 import React from "react";
 import withAuthentication from "../utils/authentication";
-import { NavBar } from "./NavBar";
+import NavBar from "./Navbar";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
+import NewQuestion from "./New_Question";
+import { ViewQuestion } from "./View_Question";
+import { LeaderBoard } from "./Leaderboard";
+import Questions from "./Questions";
+import { makeStyles } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  contentWrapper: {
+    width: "50%",
+    margin: "auto",
+  },
+}));
 const Dashboard = () => {
+  const classes = useStyles();
+  const { path } = useRouteMatch();
   return (
     <div>
       <NavBar />
-      <h1>Dashboard</h1>
+      <div className={classes.contentWrapper}>
+        <Switch>
+          <Route exact path={path} component={Questions} />
+          <Route exact path={`${path}/new-question`} component={NewQuestion} />
+          <Route exact path={`${path}/leaderboard`} component={LeaderBoard} />
+          <Route path={`${path}/:questionId`} component={ViewQuestion} />
+        </Switch>
+      </div>
     </div>
   );
 };
