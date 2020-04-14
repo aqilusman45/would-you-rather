032a88eab93fd52c24ce5ac65e026f86c66b42f2
aqilusman45 <aqilusman45@gmail.com>
2020-04-14 12:27:46 +0500

    feat: routing and get initial data logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
-import { getInitialState } from "../store/actions/shared";
+import React, { useEffect } from "react";
+import { getStateFromDB } from "../store/actions/shared";
 import { connect } from "react-redux";
-import { Questions } from "../store/actions/questions";
-import { Users } from "../store/actions/users";
-import { Switch, Route } from "react-router-dom";
+import { Loading } from "../store/actions/loading";
+import { Switch, Route, useHistory, useLocation } from "react-router-dom";
 import { Login } from "./Login.js";
-import Dashboard from "./Dashboard";
 import NotFound from "./Not_Found";
 import Alert from "@material-ui/lab/Alert";
 import { makeStyles } from "@material-ui/core/styles";
 import withAuthentication from "../utils/authentication";
 import { AuthedUser } from "../store/actions/authedUser";
 import PropTypes from "prop-types";
+import NewQuestion from "./New_Question";
+import ViewQuestion from "./View_Question";
+import LeaderBoard from "./Leaderboard";
+import Questions from "./Questions";
+import NavBar from "./NavBar";
 
 const useStyles = makeStyles((theme) => ({
   alert: {
@@ -23,26 +26,48 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function App({ dispatch, alerts }) {
+function App({ dispatch, alerts, users, questions, authedUser }) {
   const classes = useStyles();
-  useState(async () => {
-    if (localStorage.getItem("authedUser") !== null) {
-      dispatch(
-        AuthedUser.setAuthedUser(JSON.parse(localStorage.getItem("authedUser")))
-      );
+  const { push } = useHistory();
+  const { pathname } = useLocation();
+  useEffect(() => {
+    if (
+      Object.keys(users).length === 0 &&
+      Object.keys(questions).length === 0
+    ) {
+      const getData = async () => {
+        if (localStorage.getItem("authedUser") !== null) {
+          dispatch(
+            AuthedUser.setAuthedUser(
+              JSON.parse(localStorage.getItem("authedUser"))
+            )
+          );
+        }
+        const [users, questions] = await getStateFromDB();
+        dispatch(Loading.dataReceived(users, questions));
+      };
+      getData();
     }
-    const [users, questions] = await getInitialState();
-    dispatch(Questions.setQuestions(questions));
-    dispatch(Users.setUsers(users));
-  }, []);
+    return () => {};
+  }, [questions, users, dispatch, pathname, push]);
 
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <NavBar />
+      <div className="dashboard">
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path={"/questions"} component={Questions} />
+          <Route exact path={`/add`} component={NewQuestion} />
+          <Route exact path={`/leaderboard`} component={LeaderBoard} />
+          <Route
+            exact
+            path={`/questions/:question_id`}
+            component={ViewQuestion}
+          />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
       <div className={classes.alert}>
         {alerts.msg && <Alert severity="error">{alerts.msg}</Alert>}
       </div>
@@ -53,10 +78,15 @@ function App({ dispatch, alerts }) {
 App.prototype = {
   dispatch: PropTypes.func,
   alert: PropTypes.object,
+  users: PropTypes.object,
+  questions: PropTypes.object,
 };
 
-const mapStateToProps = ({ alerts }) => ({
+const mapStateToProps = ({ alerts, users, questions, authedUser }) => ({
   alerts,
+  users,
+  questions,
+  authedUser,
 });
 
 export default connect(mapStateToProps)(withAuthentication(App));
