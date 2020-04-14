7dbe726bf0eb53b6b431671b4dbafbe9bfc56e5d
aqilusman45 <aqilusman45@gmail.com>
2020-04-14 12:29:13 +0500

    feat: leaderboard data logic and UI

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,9 +1,47 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { LeadersListItem } from "./Leaders_List";
+const LeaderBoard = ({ leaders, loading }) => {
+  if (loading) {
+    return <h1 className="loading-text">Loading</h1>;
+  }
 
-export const LeaderBoard = () => {
   return (
     <div>
-      <h1>LeaderBoard</h1>
+      <ul className="leaders-list">
+        {leaders.map((leader) => (
+          <LeadersListItem key={leader.id} user={leader} />
+        ))}
+      </ul>
     </div>
   );
 };
+
+LeaderBoard.prototype = {
+  users: PropTypes.array,
+};
+
+const mapStateToProps = ({ users, loading }) => {
+  let leaders;
+
+  if (!loading) {
+    leaders = Object.values(users)
+      .map((user) => {
+        let totalScore =
+          Object.keys(user.answers).length + user.questions.length;
+        return {
+          ...user,
+          totalScore,
+        };
+      })
+      .sort((a, b) => b.totalScore - a.totalScore);
+  }
+
+  return {
+    leaders,
+    loading,
+  };
+};
+
+export default connect(mapStateToProps)(LeaderBoard);
