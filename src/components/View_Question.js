1c28334e79bc3c7e9a61a4bb920f4fbdf9831b46
aqilusman45 <aqilusman45@gmail.com>
2020-04-14 12:35:48 +0500

    feat: view based on question status

--- src/components/View_Question.js
+++ src/components/View_Question.js
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { connect } from "react-redux";
+import { questionStatus } from "../utils/handleQuestions";
+import PropTypes from "prop-types";
+import AnswerQuestion from "./Answer_Question";
+import AnsweredQuestions from "./Answered_Question";
 
-export const ViewQuestion = () => {
-  return (
-    <div>
-      <h1>Question By Id</h1>
-    </div>
-  );
+const ViewQuestion = ({ authedUser, question, status, loading }) => {
+  const { push } = useHistory();
+  useEffect(() => {
+    console.log(
+      "state",
+      loading,
+      question,
+      typeof question === "undefined" && loading
+    );
+
+    if (typeof question === "undefined" && !loading) {
+      push("/questions");
+    }
+  }, [question, push, loading]);
+
+  if (loading) {
+    return <h1 className="loading-text">loading</h1>;
+  }
+
+  if (status) {
+    return (
+      <div>
+        <AnsweredQuestions authedUser={authedUser} question={question} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <AnswerQuestion authedUser={authedUser} question={question} />
+      </div>
+    );
+  }
 };
+
+ViewQuestion.prototype = {
+  authedUser: PropTypes.object,
+  question: PropTypes.object,
+  status: PropTypes.bool,
+  loading: PropTypes.bool,
+};
+
+const mapStateToProps = (
+  { questions, authedUser, loading, users },
+  { match }
+) => {
+  let question;
+
+  if (!loading && typeof questions[match.params.question_id] !== "undefined") {
+    question = {
+      ...questions[match.params.question_id],
+      author: {
+        ...users[questions[match.params.question_id].author],
+      },
+    };
+  }
+  const status = questionStatus(question, authedUser?.id);
+
+  return {
+    authedUser,
+    question,
+    status,
+    loading,
+  };
+};
+
+export default connect(mapStateToProps)(ViewQuestion);
